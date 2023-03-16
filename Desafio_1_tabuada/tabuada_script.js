b246176df4d7f47997a61d56ac30a5659c7066f3
Filipe Alves <filipealvesrds2019@gmail.com>
2023-03-16 10:52:45 -0300

    comitando

--- Desafio_1_tabuada/tabuada_script.js
+++ Desafio_1_tabuada/tabuada_script.js
@@ -3,24 +3,31 @@ function tabuada(){
     var num = window.document.getElementById('num')
     var end = document.getElementById('end')
 
-    var n = Number(num.value)
-    var e = Number(end.value)
+    if (num.value.lenght == 0 || end.value.lenght == 0){
+        window.alert("comando  invalido tente novamente!");
+    }else{
+        var n = Number(num.value)
+        var e = Number(end.value)
 
-    var x = 0
+        var x = 0
 
-    var r = ''
+        var r = ''
 
-    var res = window.document.getElementById('resposta')
-    res.innerHTML = 'A tabuada do '+ n + ' é: <br>';
-    
-
-    //document.getElementById('resposta').innerHTML = 'A resposta e: '+ r;
-
-    do{
-        r = n*x
-        res.innerHTML +=  r + '<br>';
-        x++
-    }while(x <= e)
+        var res = window.document.getElementById('resposta')
+        res.innerHTML = 'A tabuada do ' + n + ' é: <br>';
 
 
+        //document.getElementById('resposta').innerHTML = 'A resposta e: '+ r;
+
+        do {
+            r = n * x
+            res.innerHTML += x + ' X ' + n + ' = ' + r + '<br>';
+            x++
+        } while (x <= e)
+
+        window.alert("comando  invalido tente novamente!");
+    }
+
+   
+  
 }
