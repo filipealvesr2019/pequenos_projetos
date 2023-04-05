be96cda888099670fa9ce3a63a29b18c3a821f97
Filipe Alves <filipealvesrds2019@gmail.com>
2023-04-04 21:24:04 -0300

    atualização

--- desafio CCXP/main.js
+++ desafio CCXP/main.js
@@ -6,8 +6,60 @@ function addRedBorder(id)
 
 //addRedBorder("sexta");
 
-function highlightCard(selector)
+function highlightCard(cardID)
 {
-    var Element = document.querySelector(selector);
+    var Element = document.querySelector(cardID);
     Element.classList.toggle("card-highlight");
 }
+
+function checkKeyBoardCode()
+{
+    document.addEventListener('keydown', (event) => {
+        var name = event.key;
+        var name = event.code;
+
+        alert(`tecla pressionada ${name} \r\n valor do keycode: ${code}`);
+    }, false);
+}
+
+function addKeyBoardEventListener()
+{
+    document.addEventListener('keydown', (event) => {
+        var ingresso1 = document.getElementById("quinta");
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");
+        var ingresso4 = document.getElementById("domingo");
+
+        var code = event.code;
+        if(code == 'digite1'){
+            ingresso1.classList.toggle("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'digite2'){
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.toggle("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'digite3'){
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.toggle("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'digite4'){
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.toggle("card-highlight");
+        }
+    }, false);
+}
+
+
+addKeyBoardEventListener();
