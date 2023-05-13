94a4353079c65be187e26d96414466bbbff26c1d
Filipe Alves <filipealvesrds2019@gmail.com>
2023-05-12 21:37:16 -0300

    commit

--- cafe shop/index.js
+++ cafe shop/index.js
@@ -8,3 +8,36 @@ function hideButton(card){
     var button = card.querySelector("button");
     button.classList.add('hidden');
 }
+
+
+var modal = document.getElementById("modal");
+var openModalText = document.getElementById("openModalText");
+var closeBtn = document.getElementById("close");
+var closeButton = document.getElementById("close-button");
+
+openModalText.onclick = function(){
+    modal.style.display = "block";
+    closeButton.style.display = "inline-block";
+}
+
+
+
+closeBtn.onclick = function(){
+    modal.style.display = "none";
+    closeButton.style.display = "none";
+}
+
+
+
+closeButton.onclick = function(){
+    modal.style.display = "none";
+    closeButton.style.display = "none";
+}
+
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+        closeButton.style.display = "none";
+    }
+
+}
